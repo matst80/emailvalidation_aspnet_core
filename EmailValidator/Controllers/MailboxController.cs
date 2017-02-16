6c742419df0bed1e34a7a5d0ee03690f4eacf21d
Mats Törnberg <mats.tornberg@cavagent.com>
2017-02-16 10:16:51 +0100

    sdf

---
+++ EmailValidator/Controllers/MailboxController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailValidator.Controllers
+{
+    [Route("api/[controller]")]
+    public class MailboxController : Controller
+    {
+
+        [HttpGet("{ip}/{email}")]
+        public MailBoxResult Validate(string ip, string email)
+        {
+            var ret = new MailBoxResult();
+            ret.Validate(ip,email);
+            return ret;
+        }
+
+
+    }
+}
